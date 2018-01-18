f75000139960302424fcc630278d9fb2511d9c95
LJH <dlwngud3028@gmail.net>
2018-01-18 22:01:56 +0900

    전화걸기 작업(ACTION_CALL)

---
+++ android/android_practice/Call/app/src/main/java/com/example/taehyo/call/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.taehyo.call;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void onClick(View v) {
+        Intent intent=new Intent(this ,SubActivity.class);
+        startActivity(intent);
+
+    }
+}
