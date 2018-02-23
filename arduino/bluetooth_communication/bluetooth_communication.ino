efed385a15b74a4015acdb2c3005c875c2195abf
Hochan Lee <nicelhc13@naver.com>
2018-02-23 22:56:10 +0900

    write touch pad arduino code template

--- arduino/bluetooth_communication/bluetooth_communication.ino
+++ arduino/bluetooth_communication/bluetooth_communication.ino
@@ -1,11 +1,22 @@
  #include <SoftwareSerial.h>
+ #include <Wire.h>
+ #include "Adafruit_MPR121.h"
 
 #define BUFF_SIZE 256
+
+// You can have up to 4 on one i2c bus but one is enough for testing!
+Adafruit_MPR121 cap = Adafruit_MPR121();
+
+// Keeps track of the last pins touched
+// so we know when buttons are 'released'
+uint16_t lasttouched = 0;
+uint16_t currtouched = 0;
+
 int joystick_x = A0;
 int joystick_y = A1;
 int joystick_press = 9;
-int blue_Tx = 2;  //블루투스 모듈의 T(Transmitt)x를 Digital pin 9번에 연결
-int blue_Rx = 3;  //블루투스 모듈의 R(Receive)x를 Digital pin 10번에 연결
+int blue_Tx = 7;  //블루투스 모듈의 T(Transmitt)x를 Digital pin 9번에 연결
+int blue_Rx = 4;  //블루투스 모듈의 R(Receive)x를 Digital pin 10번에 연결
 int bufferSize = 0;
 //String mac_addr = "rn00:21:13:01:51:5D\n"; // 박효정 MAC_ID
 String mac_addr = "\r20:16:05:19:90:62\n"; // 이호찬 MAC_ID
@@ -39,11 +50,23 @@ void setup(){
   digitalWrite(joystick_press, HIGH);
   BTSerial.begin(9600);
   Serial.begin(9600);
+
+  while (!Serial) {
+    delay(10);
+  }
+
+  if (!cap.begin(0x5A)) {
+    Serial.println("MPR121 not found, check wiring?");
+    while (1);
+  }
+  Serial.println("MPR121 found!");
+  
   index =0;
   bufferSize = 0;
 }
 
 void loop(){
+    checkTouchPad();
     serialMode();
     sendJoyStickInput();
     //bluetoothMode();
@@ -182,5 +205,26 @@ void setJoyStickDirection(int _top_m, int _bottom_m, int _left_m, int _right_m) 
   bottom_m = _bottom_m;
   left_m = _left_m;
   right_m = _right_m;
+}
+
+void checkTouchPad() {
+  currtouched = cap.touched();
+  
+  for (uint8_t i=0; i<12; i++) {
+    // it if *is* touched and *wasnt* touched before, alert!
+    if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
+      Serial.print(i); Serial.println(" touched");
+    }
+    // if it *was* touched and now *isnt*, alert!
+    if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
+      Serial.print(i); Serial.println(" released");
+    }
+  }
+  
+  // reset our state
+  lasttouched = currtouched;
+
+  // comment out this line for detailed data from the sensor!
+  return;
 }
 
